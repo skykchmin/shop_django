7c5afc49b1414650a54ae14a2468dacc70393ba9
skykchmin <skykchmin@naver.com>
2020-03-20 23:22:23 +0900

    shop_django 프로젝트 구성
    shop_django 프로젝트 구성
    
    초기세팅

---
+++ store_venv/store_env/store_django/order/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Order(models.Model):
+    stuser = models.ForeignKey('stuser.Stuser', on_delete=models.CASCADE, verbose_name='사용자') # on_delete 사용자가 삭제되었을 떄 order는 어떻게 할 것인가
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='상품')
+    quantity = models.IntegerField(verbose_name='수량')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
---
+++ store_venv/store_env/store_django/stuser/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Stuser(models.Model):
+    email = models.EmailField(verbose_name='이메일')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        db_table = 'store_stuser'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자'
