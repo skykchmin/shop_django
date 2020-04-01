9845a8330d95ebe740f3adeae064f84d8d4047fd
skykchmin <skykchmin@naver.com>
2020-04-01 23:49:35 +0900

    주문하기 화면 , Valueerror(미해결)
    주문하기 화면 , Valueerror(미해결)

--- store_venv/store_env/store_django/product/views.py
+++ store_venv/store_env/store_django/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
-
+from order.forms import RegisterForm as OrderForm
 # from django.views.generic import FormView
 # from .forms import RegisterForm
 # Create your views here.
@@ -25,4 +25,7 @@ class ProductDetail(DetailView):
     queryset = Product.objects.all() # 어떤 모델을 지정하는게 아니라 쿼리셋을 지정
     context_object_name = 'product'
     
-    
+    def get_context_data(self, **kwangs): # detailform 에 전달되는 게 필요, 내가 원하는 데이터를 집어넣을 수 있게
+        context = super().get_context_data(**kwangs) # 먼저 디테일 view가 원하는 것을 만든 다음
+        context['form'] = OrderForm()
+        return context
